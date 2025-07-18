00022b36da6b537b47a8fd574b0d45ee4cddb804
Mahad Malik <65032636+mahad-malik@users.noreply.github.com>
2025-07-18 12:05:08 +0200

    Update list_models.py

--- list_models.py
+++ list_models.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyClCQXNPai8tbT8rM3L6tVnC-WYF69PE2g")
+genai.configure(api_key="API key")
 
 models = list(genai.list_models())
 for model in models:
